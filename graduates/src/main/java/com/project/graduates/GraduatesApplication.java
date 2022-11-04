a6d58dc344300f8865c94e197ed2e4a3c0e4e9e2
giusencito <66499088+giusencito@users.noreply.github.com>
2022-11-04 14:44:04 -0500

    creación del proyecto

---
+++ graduates/src/main/java/com/project/graduates/GraduatesApplication.java
@@ -0,0 +1,13 @@
+package com.project.graduates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduatesApplication.class, args);
+	}
+
+}
